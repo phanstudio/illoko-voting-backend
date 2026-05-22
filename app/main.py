a1128b91ff2014274cb297ac1d8590273a2efc0b
phanstudio <85735876+phanstudio@users.noreply.github.com>
2026-05-22 15:47:18 +0100

    update

--- app/main.py
+++ app/main.py
@@ -70,9 +70,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://illoko-voting-platform.netlify.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
